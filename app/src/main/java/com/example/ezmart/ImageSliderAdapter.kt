ba3533e592a29a119bd601e9956b38ae9d1aaae7
Charlie Martinez <chse.martinez.up@phinmaed.com>
2025-02-20 06:44:53 +0800

    added bottom navigation and viewpager ads

---
+++ app/src/main/java/com/example/ezmart/ImageSliderAdapter.kt
@@ -0,0 +1,25 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ezmart.R
+
+class ImageSliderAdapter(private val imageList: List<Int>) :
+    RecyclerView.Adapter<ImageSliderAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageList[position])
+    }
+
+    override fun getItemCount(): Int = imageList.size
+}
--- app/src/main/java/com/example/ezmart/MainActivity.kt
+++ app/src/main/java/com/example/ezmart/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.ezmart
 
+import ImageSliderAdapter
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.selectedItemId = R.id.nav_home
+
+        val imageList = listOf(R.drawable.ad1, R.drawable.ad2, R.drawable.ad3)
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val tabIndicator: TabLayout = findViewById(R.id.tabIndicator)
+
+        val adapter = ImageSliderAdapter(imageList)
+        viewPager.adapter = adapter
+
+        // Attach the dots indicator to the ViewPager
+        TabLayoutMediator(tabIndicator, viewPager) { _, _ -> }.attach()
+
     }
 }
